c2ede10acf0cbb25dcea863a19ca93177d5457f9
yug614 <yugginoya13@gmail.com>
2025-05-06 14:50:51 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+    float angle1, angle2, angle3;
+
+    
+    printf("Enter the first angle (in degrees): ");
+    scanf("%f", &angle1);
+
+    printf("Enter the second angle (in degrees): ");
+    scanf("%f", &angle2);
+
+    
+    angle3 = 180.0 - (angle1 + angle2);
+
+   
+    printf("The third angle is: %.2f degrees\n", angle3);
+
+   
+}
+
+
+/* input
+   first angle is 60
+   second one is 70
+   output:50
+*/
