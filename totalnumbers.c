5736a3723925f487a931772ad76ad32e0e0bcc17
yug614 <yugginoya13@gmail.com>
2025-05-15 17:43:16 +0530

    Add files via upload

---
+++ totalnumbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int sum=0, count, num;
+	
+	printf("enter the num: ");
+	scanf("%d", &num);
+	
+	
+	while(num>0)
+	{
+		num=num/10;
+		count++;
+	}
+	printf("%d",count);
+}
