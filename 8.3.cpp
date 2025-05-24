e176e859a74be0bcc8e128e5336f9a78d11a1489
yug614 <yugginoya13@gmail.com>
2025-05-24 17:02:50 +0530

    Update 8.3.cpp

--- 8.3.cpp
+++ 8.3.cpp
@@ -1,28 +1,44 @@
 #include<stdio.h>
 int main()
 {
-	int size,i=0;
+	int size,i=0 ;
 	
-	printf("enter array size: ");
+	printf("enter array size for a: ");
 	scanf("%d", &size);
 	
-	int a[size];
+	int a[size],b[size],c[size];
 	
-	printf("enter array eliments: \n");
-	
-	
-	int b[size];
-	
-	printf("enter array eliments: \n");
+	printf("enter array a's eliments: \n");
 	
 	for(i; i<=size; i++)
 	{
 		printf("a[%d]= ",  i);
 		scanf("%d", &a[i]);
-		sum+= a[i];
+		
 	}
 	
-	printf("length of an array:%d\n", size);
+	
+	printf("enter array b's eliments: \n");
+	
+	for(i; i<=size; i++)
+	{
+		printf("b[%d]= ",  i);
+		scanf("%d", &b[i]);
+		
+	}
+
+	
+	for(i; i<=size; i++)
+	{
+		c[i]= a[i] + b[i];
+	}
+	 
+	printf("new array c is: \n");
+		
+	for(i; i<=size; i++)
+	{
+		printf("%d", c[i]);
+	}
 	
 
 }
